78228095bba673b97fa381741a7b08f2e3898fb4
thatsOven <68780017+thatsOven@users.noreply.github.com>
2020-10-24 20:23:01 +0200

    first source, isn't finished yet

--- Python/GrailSort.py
+++ Python/GrailSort.py
@@ -29,9 +29,9 @@ def arrayCopy(fromArray, fromIndex, toArray, toIndex, length):
  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  # SOFTWARE.
- #/
  #
- # <name-of-holy-grail-project>
+ #
+ # The Holy Grail Sort Project
  # Project Manager:      Summer Dragonfly
  # Project Contributors: 666666t
  #                       Anonymous0726
