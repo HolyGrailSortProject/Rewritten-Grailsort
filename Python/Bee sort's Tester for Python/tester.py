d7fa1ab8dadad34b981a10e1e85987ab613b41ad
YusrilIhsan-codes <yusrilarfan2@gmail.com>
2020-11-17 21:22:43 +0700

    Create tester.py

---
+++ Python/Bee sort's Tester for Python/tester.py
@@ -0,0 +1,45 @@
+#
+ # MIT License
+ # 
+ # Copyright (c) 2013 Andrey Astrelin
+ # Copyright (c) 2020 The Holy Grail Sort Project
+ # 
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documentation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to whom the Software is
+ # furnished to do so, subject to the following conditions:
+ # 
+ # The above copyright notice and this permission notice shall be included in all
+ # copies or substantial portions of the Software.
+ # 
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ # SOFTWARE.
+ #
+ #
+ # The Holy Grail Sort Project
+ # Project Manager:      Summer Dragonfly
+ # Project Contributors: 666666t
+ #                       Anonymous0726
+ #                       aphitorite
+ #                       dani_dlg
+ #                       EilrahcF
+ #                       Enver
+ #                       lovebuny
+ #                       MP
+ #                       phoenixbound
+ #                       thatsOven
+ #                       Bee sort
+ #                       _fluffyy
+ #                       Morwenn
+ #                       
+ # Special thanks to "The Studio" Discord community!
+ #
+# Editor : Bee sort
+# tester.py is currently empty now. I'll look on further on how to set this up.
