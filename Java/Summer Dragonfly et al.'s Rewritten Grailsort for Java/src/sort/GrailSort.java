740fde59121016b6eac23831f7c58bf1b3723e61
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-05-03 04:33:10 -0500

    Update copyright in Summer Dragonfly et al. implementation

--- Java/Summer Dragonfly et al.'s Rewritten Grailsort for Java/src/sort/GrailSort.java
+++ Java/Summer Dragonfly et al.'s Rewritten Grailsort for Java/src/sort/GrailSort.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
  * MIT License
  * 
  * Copyright (c) 2013 Andrey Astrelin
- * Copyright (c) 2020 The Holy Grail Sort Project
+ * Copyright (c) 2020-2021 The Holy Grail Sort Project
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
